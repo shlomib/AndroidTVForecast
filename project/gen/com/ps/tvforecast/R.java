eaa33e33c83e4252726ba4183c1a2421f6a782e8
Shlomo Bihari <shlomib@gmail.com>
2013-11-04 19:48:28 -0800

    + adding tv icon

--- project/gen/com/ps/tvforecast/R.java
+++ project/gen/com/ps/tvforecast/R.java
@@ -40,6 +40,7 @@ containing a value of this type.
     public static final class drawable {
         public static final int ic_add=0x7f020000;
         public static final int ic_launcher=0x7f020001;
+        public static final int ic_tv=0x7f020002;
     }
     public static final class id {
         public static final int action_search=0x7f08000d;
